8fbaa6a92ecf318acf0b3d811ef0bacd3ac45300
Fuzi18 <fuzesi.t2003@gmail.com>
2022-09-28 14:31:46 +0200

    s

--- src/index.js
+++ src/index.js
@@ -9,11 +9,25 @@ function kep(){
 }
 function szelesseg() {
   let szelesseg = document.getElementById("szelesseg").value;
-  document.getElementById("")
+  document.getElementById("kep").style.width = szelsseg + "px";
+}
+function vastagsag() {
+  let vastag = document.getElementById("vastagsag").value;
+  document.getElementById("kep").style.borderWidth = vastag + "px";
+  console.log(vastag);
+  
+}
+function szin() { 
+  let szinek = document.getElementById("szin").value;
+  document.getElementById("kep").style.borderColor = szinek;
 }
 
 function klikk() {
   
 }
+document.getElementById("url").addEventListener('change', kep);
+document.getElementById("szelesseg").addEventListener('change', szelesseg);
+document.getElementById("vastagsag").addEventListener('change', vastagsag);
+document.getElementById("szin").addEventListener('input', szin);
 
 
