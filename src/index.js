c114cc544b92bf52ac94b1237ff144beb82f71f9
Fuzi18 <fuzesi.t2003@gmail.com>
2022-09-28 14:02:27 +0200

    s

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
 
+function kep(){
+  let link = document.getElementById("url").value;
+  
+
+}
+
+function klikk() {
+  
+}
+
 
